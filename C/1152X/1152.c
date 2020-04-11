6d3b50a5389f44e73b36890ee70db2db0ee28906
윤성주 <>
2020-04-11 23:36:42 +0900

    d

--- C/1152X/1152.c
+++ C/1152X/1152.c
@@ -1,27 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-	char c[1000000];
-	int count=0;
-	int num=0;
-	int k=0;
-
-	scanf("%s",c);
-
-	if ((k=c[num])==32){
-		while ((k=c[num])!=32){
-			num++;
-		}
-	}else{
-		while ((k=c[num])!=-1){
-			if ((k=c[num])==32){
-				if ((k=c[num])==32 || (k=c[num])==-1){
-					break;
-				}
-			}
-
-		}
+int main() {
+	unsigned int counter = 0;
+	char str[1000000];
+	char* ptr;
+	
+	while (scanf("%s", str) != EOF)
+	{
+		counter++;
 	}
-	printf("%d",count);
+	printf("%d\n", counter);
 	return 0;
 }
